fda66f234f9c9d363e86e62c4e6e424d25ca2534
elmon1987 <elmon1987@users.noreply.github.com>
2015-04-15 15:02:45 +0700

    Update main.cpp

--- Binary File/main.cpp
+++ Binary File/main.cpp
@@ -1,8 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
 #include <iostream>
-#include <math.h>
+#include <RandomClass.h>
+#include <RandomGenerator.h>
 
 using namespace std;
 
@@ -13,44 +11,6 @@ typedef char MB_BLOCK[size];
 const char* readbin = "rb";
 const char* writebin = "wb";
 const char* input = "data.bin";
-const int K = 100;
-const float c = 0.1;
-const double delta = 0.5;
-const double S = c*log((double)K/delta)*sqrt((double)K);
-
-double M[K+1];
-
-class Random {
-private:
-    uint32_t M ;
-    uint32_t A ;
-    uint32_t state;
-    uint32_t MAX_RAND ;
-public:
-    Random(): M(2147483647UL), A(16807), MAX_RAND(M-1)
-    {}
-    void setSeed(uint32_t S)
-    {
-        state = S;
-    }
-
-    uint32_t getState()
-    {
-        return state;
-    }
-
-    uint32_t getMAX_RAND()
-    {
-        return MAX_RAND;
-    }
-
-    uint32_t nextInt()
-    {
-        uint32_t next = (uint32_t)(((uint64_t)state * A) % M);
-        state = next;
-        return next;
-    }
-};
 
 void MakeFile(int filesize)
 {
@@ -76,58 +36,7 @@ void SplitBlock(int filesize)
     //    cout << data[2][i];
     //cout << "DONE";
 }
-double ISD(int i)     /// Ideal Soliton Distribution
-{
-    if(i==1)
-        return 1/(double)K;
-    return 1/(double)(i*(i-1));
-}
 
-double NNF(int i)    /// non-negative function
-{
-    if (i < (int)((double)K/S))
-        return S/(double)((double)K*(double)i);
-    else if ( i == (int)((double)K/S) )
-            return S*log(S/delta)/(double)K;
-    return 0;
-}
-
-double RSD(int i)    ///  Robust Soliton Distribution
-{
-    return ISD(i)+NNF(i);
-}
-
-void InitCDF(double M[])   /// Generate Array M
-{
-    double Z = 1;
-    for (int i = 1; i<=K; ++i)
-        Z+= NNF(i);
-    M[0] = 0;
-    for (int i = 1; i<=K; ++i)
-        M[i]=M[i-1] + RSD(i)/Z;
-}
-
-int BinarySearch(double M[], int n, double u)
-{
-    int left = 0, right = n, mid;
-    do
-    {
-        mid = (left + right)/2;
-        if ( M[mid] < u )
-            if ( M[mid+1] >= u )
-                return mid+1;
-            else
-                left = mid + 1;
-        else
-        if ( M[mid] > u )
-            if ( M[mid-1] <= u )
-                return mid;
-            else
-                right = mid - 1;
-    }
-    while(left <= right);
-    return mid;
-}
 
 int main()
 {
