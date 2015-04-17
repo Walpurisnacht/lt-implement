0f2432f9ef1fefb59ade715dae92102e83bf454d
elmon1987 <elmon1987@users.noreply.github.com>
2015-04-18 00:45:47 +0700

    Update main.cpp

--- Binary File/main.cpp
+++ Binary File/main.cpp
@@ -20,27 +20,5 @@ void MakeFile(int filesize)
 
 int main()
 {
-    /*int mb;
-    cout << "Input file size: ";
-    cin >> mb;*/
-    //-----Make MB-----//
-    //MakeFile(100);
-    //system("pause");
-    //-----Test random-----//
-    Random pseudo;
-    pseudo.setSeed(2067261);
-    //cout << tmp << " " << (double) tmp/pseudo.getMAX_RAND() << endl;
-    //double t = (double)tmp/pseudo.getMAX_RAND();
-    //cout << t << endl;
-    InitCDF(M);
-    //for (int i = 1; i <= K; ++i)
-        //cout << M[i] <<endl;
-    for (int i = 1; i<= K; ++i)
-    {
-        uint32_t tmp = pseudo.nextInt();
-        double t = (double)tmp/pseudo.getMAX_RAND();
-        cout << BinarySearch(M,K,t) << endl;
-    }
-    //-----Split block-----//
-    //SplitBlock(100);
+   Encoding();
 }
